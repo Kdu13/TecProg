01093c9b7d46815f7a9ea46bd404693407e924cc
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-08-15 21:39:28 -0300

    ,

--- src/atividades/atividadepratica02/exercicio03.java
+++ src/atividades/atividadepratica02/exercicio03.java
@@ -1,5 +1,28 @@
 package src.atividades.atividadepratica02;
 
+import java.util.Scanner;
+
 public class exercicio03 {
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int[][] matrizTrocada = new int[3][3];
+
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.print("Add na linha " + l + " e coluna " + c + ": ");
+                matriz[l][c] = scan.nextInt();
+            }
+        }
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+                
+                matrizTrocada[l][c] = matriz[c][l];
+                System.out.print(matrizTrocada[l][c] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
