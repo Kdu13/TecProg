986af4a754c98d0ea00fda384085cd12e239e14c
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-08-15 21:35:15 -0300

    .

---
+++ src/atividades/atividadepratica02/exercicio01.java
@@ -0,0 +1,40 @@
+package src.atividades.atividadepratica02;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[][] matrizA = new int[3][3];
+        int[][] matrizB = new int[3][3];
+        int[][] somaMatriz = new int[3][3];
+
+        System.out.println("Add matriz 1");
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.println("Add o num na linha " + l + " e a coluna " + c);
+                matrizA[l][c] = scan.nextInt();
+            }
+        }
+        System.out.println("Add matriz 2");
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.println("Add o num na linha " + l + " e a coluna " + c);
+                matrizB[l][c] = scan.nextInt();
+            }
+        }
+        System.out.println("Soma");
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                somaMatriz[l][c] = (matrizA[l][c]) + (matrizB[l][c]);
+                System.out.print(somaMatriz[l][c] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
