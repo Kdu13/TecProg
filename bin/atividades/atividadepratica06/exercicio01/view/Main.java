b27b4ec074cd562d55ad62b572d44b45502c29ff
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-09-12 15:43:57 -0300

    .

--- bin/atividades/atividadepratica06/exercicio01/view/Main.java
+++ bin/atividades/atividadepratica06/exercicio01/view/Main.java
@@ -1,5 +1,10 @@
 package bin.atividades.atividadepratica06.exercicio01.view;
+import bin.atividades.atividadepratica06.exercicio01.model.Loja;
 
 public class Main {
     
+    public static void main(String[] args) {
+        
+
+    }
 }
