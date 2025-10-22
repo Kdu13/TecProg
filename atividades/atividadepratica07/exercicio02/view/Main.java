aae8450448af937155d39df3e90c1c03334e55e2
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-10-22 14:55:19 -0300

    Update Main.java

--- atividades/atividadepratica07/exercicio02/view/Main.java
+++ atividades/atividadepratica07/exercicio02/view/Main.java
@@ -5,11 +5,11 @@ import atividades.atividadepratica07.exercicio02.model.PagamentoDinheiro;
 public class Main {
     public static void main(String[] args) {
         
-        PagamentoDinheiro reais = new PagamentoDinheiro(500, 0.1);
+        PagamentoDinheiro reais = new PagamentoDinheiro(100, 0.1);
         System.out.println(reais.calcularPagamento());
         System.out.println(reais.emitirRecibo());
         System.out.println();
-        PagamentoCartao nubank = new PagamentoCartao(500, 0.05);
+        PagamentoCartao nubank = new PagamentoCartao(100, 0.05);
         System.out.println(nubank.calcularPagamento());
         System.out.println(nubank.emitirRecibo());
 
