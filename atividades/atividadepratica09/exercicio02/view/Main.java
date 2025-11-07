6c25a50c283d089a0cddc2274fd03973ff45742c
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-11-07 11:05:48 -0300

    Update Main.java

--- atividades/atividadepratica09/exercicio02/view/Main.java
+++ atividades/atividadepratica09/exercicio02/view/Main.java
@@ -4,6 +4,6 @@ import atividades.atividadepratica09.exercicio02.model.PreferenciasUsuario;
 
 public class Main {
     public static void main(String[] args) {
-            new PreferenciasUsuario();
+        new PreferenciasUsuario();
     }
 }
