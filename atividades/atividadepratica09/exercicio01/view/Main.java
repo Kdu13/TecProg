2cae634c2d53b948aa41eece17218d974533c203
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-11-07 11:05:13 -0300

    Update Main.java

--- atividades/atividadepratica09/exercicio01/view/Main.java
+++ atividades/atividadepratica09/exercicio01/view/Main.java
@@ -4,8 +4,6 @@ import atividades.atividadepratica09.exercicio01.model.CadastroForm;
 
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new CadastroForm();
-        });
+        new CadastroForm();
     }
 }
