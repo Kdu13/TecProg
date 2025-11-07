800199f646b81b869eb7d8b69ca81f569216db61
Carlos_Eduardo <carlos.ksl2001@gmail.com>
2025-11-07 11:05:25 -0300

    Update Main.java

--- atividades/atividadepratica09/exercicio02/view/Main.java
+++ atividades/atividadepratica09/exercicio02/view/Main.java
@@ -4,8 +4,6 @@ import atividades.atividadepratica09.exercicio02.model.PreferenciasUsuario;
 
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
             new PreferenciasUsuario();
-        });
     }
 }
